7bd846a592736c718c933c527aaa4632921e1ce9
AnaAdil4462 <90888657+AnaAdil4462@users.noreply.github.com>
2022-01-12 14:49:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+// Create canvas variable
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82')
+	{
+	console.log(rr1.jpg);
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+	console.log()
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		console.log(gr.png)
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+	    console.log(pr.png)
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	   console.log(br.png)
+	}
+	
+}
+
